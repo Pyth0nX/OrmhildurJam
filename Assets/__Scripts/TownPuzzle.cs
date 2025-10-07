7eadf3d27a0b34b593e5e1cd50aa97027cc5d9de
Sarisa <sarisagaming@gmail.com>
2025-10-07 17:50:58 +0200

    TowPuzzle (working)
    puzzle and the count work now

--- Assets/__Scripts/TownPuzzle.cs
+++ Assets/__Scripts/TownPuzzle.cs
@@ -3,10 +3,24 @@ using TMPro;
 
 public class TownSeagull : MonoBehaviour
 {
-    public string tagToDisappear = "Seagull"; 
-    public int SeagullCount = 6;
-    public int disappearCount = 0;
-    public TextMeshProUGUI countText;
+    public string tagToDisappear = "Seagull";
+
+    public static int SeagullCount = 6;
+    public static int disappearCount = 0;
+    public static TextMeshProUGUI countText;
+
+    // Assign this in the Inspector only once (e.g., on the first seagull)
+    public TextMeshProUGUI referenceText;
+
+    void Start()
+    {
+        // Only assign countText once
+        if (countText == null && referenceText != null)
+        {
+            countText = referenceText;
+            UpdateCountText();
+        }
+    }
 
     void OnMouseDown()
     {
@@ -24,10 +38,5 @@ public class TownSeagull : MonoBehaviour
         {
             countText.text = $"Seagulls left: {SeagullCount - disappearCount}";
         }
-    }
-
-    void Start()
-    {
-        UpdateCountText();
     }
 }
