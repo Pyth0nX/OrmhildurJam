a2547a90835643a50b76eef83f3e9f4a832cec3e
Humar <hilmirsg@gmail.com>
2025-10-08 09:08:12 +0200

    LVL 2 Changes and PlayerState Script

---
+++ Assets/__Scripts/PlayerState.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerState : MonoBehaviour
+{
+    [SerializeField] private int mushrooms;
+    public int MushroomCount => mushrooms;
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
