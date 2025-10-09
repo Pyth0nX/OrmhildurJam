813676226ab324b4b068c163b82641e8abc62825
Humar <hilmirsg@gmail.com>
2025-10-09 12:45:35 +0200

    DIalogue Prefab

--- Assets/__Scripts/Dialogue/CaveDialogue.cs
+++ Assets/__Scripts/Dialogue/CaveDialogue.cs
@@ -13,9 +13,44 @@ public struct CaveDialogue : IDialogueEntry
         
         new DialogueEntry
         {
-            name = "name",
-            line = "dialogue",
+            name = "Ormhildur",
+            line = "Yeah, what do you need?",
+            isPlayer = true,
+        },
+        
+        new DialogueEntry
+        {
+            name = "Slug",
+            line = "I am in need of some mushrooms, and despite my size, I cannot collect them myself as the exit's blocked.",
             isPlayer = false,
+        },
+        
+        new DialogueEntry
+        {
+            name = "Slug",
+            line = "So could you collect some for me?",
+            isPlayer = false,
+        },
+        
+        new DialogueEntry
+        {
+            name = "Ormhildur",
+            line = "Aww, I'm so sorry, of course I'll help you!",
+            isPlayer = true,
+        },
+        
+        new DialogueEntry
+        {
+            name = "Slug",
+            line = "Wonderful, I will need no more than 37 mushrooms.",
+            isPlayer = false,
+        },
+        
+        new DialogueEntry
+        {
+            name = "Ormhildur",
+            line = "I'm on it!",
+            isPlayer = true,
         },
     };
 }
